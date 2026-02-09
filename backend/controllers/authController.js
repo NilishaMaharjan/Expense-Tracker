20e72942451448d4c1300b851892fc3e1bae57e7
Nilisha Maharjan <nili.mhj@gmail.com>
2026-02-09 21:10:04 +0545

    setup backend with JWT auth, secure password hashing, and exceljs integration

---
+++ backend/controllers/authController.js
@@ -0,0 +1,75 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+
+// Generate JWT token 
+const generateToken = (id) => {
+return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: '1h'});
+};
+
+// Register User
+exports.registerUser = async (req, res) => {
+    const {fullName, email, password, profileImageUrl} = req.body;
+
+    // Validation: Check for missing fields
+    if (!fullName || !email || !password) {
+        return res.status(400).json({message: 'Please provide all required fields'});
+    } 
+
+    try{
+        // Check if user already exists
+        const existingUser = await User.findOne({email});
+        if (existingUser) {
+            return res.status(400).json({message: 'User already exists'});
+        }
+
+        // Create new user
+        const user = await User.create({fullName, email, password, profileImageUrl,});
+
+        res.status(201).json({
+            _id: user._id,
+            user,
+            token: generateToken(user._id),
+        });
+    } catch(err) {
+        res.status(500).json({message: 'Server error', error: err.message});
+    }
+};
+
+// Login User
+exports.loginUser = async (req, res) => {
+    const {email, password} = req.body;
+
+    // Validation: Check for missing fields
+    if (!email || !password) {
+        return res.status(400).json({message: 'Please provide email and password'});
+    }
+    try{
+        const user = await User.findOne({email});
+        if (!user || !(await user.comparePassword(password))) {
+            return res.status(400).json({message: 'Invalid credentials'});
+        }
+
+        res.status(200).json({
+            _id: user._id,
+            user,
+            token: generateToken(user._id),
+        });
+    } catch(err) {
+         res.status(500).json({message: 'Server error', error: err.message});
+    }
+};
+
+// Get User Info
+exports.getUserInfo = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');  
+
+        if (!user) {
+            return res.status(404).json({message: 'User not found'});
+        }
+        res.status(200).json(user);
+    } catch(err) {
+        res.status(500).json({message: 'Server error', error: err.message});
+    }
+};
