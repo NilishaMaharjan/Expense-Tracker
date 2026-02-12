604f59c90b6011f5da1b8897f9c0262bfb0ff78a
Nilisha Maharjan <nili.mhj@gmail.com>
2026-02-12 14:41:22 +0545

    created income and expenses api

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,9 +2,10 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 exports.protect = async (req, res, next) => {
-    let token = req.headers.authorization?.split('')[1];
+    let token = req.headers.authorization?.split(' ')[1];
     if (!token)
         return res.status(401).json({ message: 'Not authorized, no token' });
+
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = await User.findById(decoded.id).select('-password');
@@ -12,4 +13,4 @@ exports.protect = async (req, res, next) => {
     } catch (err) {
         res.status(401).json({ message: 'Not authorized, token failed' });
     }
-};   
+};
